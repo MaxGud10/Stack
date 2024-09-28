a7ab10533c890f8a5e285f47729b924aa073efef
MaxGud10 <karatemaksim0110@gmail.com>
2024-09-29 00:43:19 +0300

    добавил толстых канарейк

--- draft_stack.cpp
+++ draft_stack.cpp
@@ -15,23 +15,35 @@
     #define DBG(...)
 #endif
 
+// #define STACK_CTOR(stack, capacity) Stack_Information (stack, capacity, #stack, __FILE__, __LINE__)
 #define STACK_CTOR(stack, capacity) stack_ctor (stack, capacity, #stack, __FILE__, __LINE__)
 #define STACK_PUSH(stack, elem)     stack_push (stack, elem, __func__, __LINE__)
 #define STACK_POP(stack, elem)      stack_pop (stack, elem, __func__, __LINE__)
 #define STACK_DUMP(stack)           stack_dump (stack, __func__, __LINE__)
 
 typedef double Stack_Elem_t; 
+typedef unsigned long long canary_t;
 
-struct Stack
+struct Stack_Information
 {
     DBG (const char* name;)
     DBG (const char* file;)
     DBG (int line;)
+};
+
+struct Stack
+{
+    DBG (canary_t left_canary;)
 
     Stack_Elem_t* data;  
     int size;      // количество элементов записывается в стек  
     int capacity; // емкость (максимальный размер буфера)
+    Stack_Information info; 
+
+    DBG (canary_t right_canary;)
 };
+
+#define CANARY_VALUE (0xDEADC0DEDEADC0DE)
 
 int  stack_ctor (struct Stack* stack, int capacity, DBG (const char* name, const char* file, int line));
 void stack_dump (struct Stack* stack, const char* function, int line);
@@ -39,9 +51,12 @@ int  stack_push (struct Stack* stack, Stack_Elem_t elem, const char* function, i
 int  stack_pop  (struct Stack* stack, Stack_Elem_t* elem, const char* function, int line);
 int  stack_assertFunc (struct Stack* stack); // верификатор 
 int  stack_destroy (struct Stack* stack);
+int  stack_error (struct Stack* stack);
 
 int main (void)
 {
+    printf("\n------------------------------------------------------------------------------\n");
+
     struct Stack stack = {};
  
     if (STACK_CTOR (&stack, 4) != 0)
@@ -50,28 +65,24 @@ int main (void)
         return 1;
     }
 
-    //stack_push (&stack, 1, "main",  __LINE__); // define main, __LINE__
     STACK_PUSH (&stack, 1);
     STACK_PUSH (&stack, 2);
     STACK_PUSH (&stack, 2.2);
     STACK_PUSH (&stack, 4.5);
-    // stack_push (&stack, 2, "main",  __LINE__);
-    // stack_push (&stack, 2.2, "main", __LINE__);
-    // stack_push (&stack, 4.5, "main", __LINE__);
 
     STACK_DUMP (&stack);
-    // stack_dump (&stack, "main", __LINE__);
 
     Stack_Elem_t x = 0;
-    STACK_POP (&stack, &x);
-    // stack_pop (&stack, &x, "main", __LINE__);
-
+    if (STACK_POP (&stack, &x) == 0)
+    {
+        printf("Popped element: %.2f\n", x);
+    }
     
     STACK_DUMP (&stack);
-    // stack_dump (&stack, "main", __LINE__);
 
     stack_destroy (&stack);
 
+    printf("\n------------------------------------------------------------------------------\n");
 
     return 0;
 }
@@ -80,15 +91,19 @@ int stack_ctor (struct Stack* stack, int capacity, DBG (const char* name, const 
 {
     assert(stack);
 
-    stack -> name = name;
-    stack -> file = file;
-    stack -> line = line;
+    stack -> left_canary = CANARY_VALUE;
 
-    stack -> data = (Stack_Elem_t*) realloc (stack -> data, stack -> capacity * sizeof(Stack_Elem_t));
+    stack -> info.name = name;
+    stack -> info.file = file;
+    stack -> info.line = line;
+
+    stack -> data = (Stack_Elem_t*) realloc (stack -> data, capacity * sizeof(Stack_Elem_t));
     if (stack -> data == NULL) return -1; 
 
     stack -> capacity = capacity;
-    //stack -> size     = 0;
+    stack -> size     = 0;
+
+    stack->right_canary = CANARY_VALUE; 
 
     return 0;
 }
@@ -97,7 +112,7 @@ void stack_dump (struct Stack* stack, const char* function, int line)
 {
     printf("\nStack dump: size = %d, capacity = %d\n", stack -> size, stack -> capacity);
     printf("\nStack[0x%p] called from %s: %d (stack_push) name %s born at %s: %d\n",
-            stack, function, line, stack -> name, stack -> file, stack -> line);
+            stack, function, line, stack -> info.name, stack -> info.file, stack -> info.line);
 
     for (int i = 0; i < (stack -> capacity); i++)
     {
@@ -107,7 +122,10 @@ void stack_dump (struct Stack* stack, const char* function, int line)
 
 int stack_push(struct Stack* stack, Stack_Elem_t elem, const char* function, int line) // TODO как сделаю верификатор. переделай функцию
 {
-    if (stack->size >= stack->capacity) 
+    assert(stack);
+    stack_assertFunc(stack);
+
+    if ((stack -> size) >= (stack -> capacity)) 
     {
         stack -> capacity *= 2;
         stack -> data = (Stack_Elem_t*) realloc (stack -> data, stack -> capacity * sizeof(Stack_Elem_t));
@@ -116,21 +134,30 @@ int stack_push(struct Stack* stack, Stack_Elem_t elem, const char* function, int
 
     stack -> data[stack->size++] = elem;
 
+    stack_assertFunc(stack);
+
     return 0;
 }
 
 int stack_pop(struct Stack* stack, Stack_Elem_t* elem, const char* function, int line)
 {
-    if (stack -> size == 0) return -1;
+    assert(stack);
+    stack_assertFunc(stack);
+
+    if ((stack -> size) == 0) return -1;
 
     *elem = stack -> data[--stack->size];
+
+    stack_assertFunc(stack);
 
     return 0; 
 }
 
 int stack_error(struct Stack* stack)
 {
-    if (stack -> size < 0)
+    if (stack -> size < 0 || 
+        stack -> left_canary  != CANARY_VALUE || 
+        stack -> right_canary != CANARY_VALUE)
         return -1;
 
     return 0;
@@ -140,7 +167,7 @@ int stack_assertFunc(struct Stack* stack) // верификатор
 {
     if (!stack_error(stack))
     {
-        STACK_DUMP (stack);
+        stack_dump(stack, "stack_assertFunc", __LINE__);
         //stack_dump (stack, "stack_assertFunc",__LINE__);
         assert(0);
     }
@@ -148,11 +175,10 @@ int stack_assertFunc(struct Stack* stack) // верификатор
     return 0;
 }
 
-
 int stack_destroy (struct Stack* stack)
 {
     free (stack->data);
-    stack -> size     = 0;    
+    stack -> size      = 0;    
     stack ->  capacity = 0; 
 
     return 0;
